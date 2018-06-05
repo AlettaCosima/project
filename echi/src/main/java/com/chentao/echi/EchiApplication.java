5073758d4ded150ceb6de65cfd86d98c2a3b7a3a
AlettaCosima <chentao0310@126.com>
2018-06-05 09:31:42 +0800

    eureka

--- echi/src/main/java/com/chentao/echi/EchiApplication.java
+++ echi/src/main/java/com/chentao/echi/EchiApplication.java
@@ -2,8 +2,10 @@ package com.chentao.echi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EchiApplication {
 
 	public static void main(String[] args) {
