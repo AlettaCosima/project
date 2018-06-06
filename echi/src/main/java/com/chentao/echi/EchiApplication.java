0569960f2f4b201641bf0346d7f4ae11a3a7a747
AlettaCosima <chentao0310@126.com>
2018-06-06 14:37:23 +0800

    eureka

--- echi/src/main/java/com/chentao/echi/EchiApplication.java
+++ echi/src/main/java/com/chentao/echi/EchiApplication.java
@@ -1,14 +1,26 @@
 package com.chentao.echi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope
+@RestController
 @SpringBootApplication
 @EnableEurekaClient
 public class EchiApplication {
+	
+	@Value("${neo}")
+	private String password;
+	
+	@RequestMapping("/")
+    public String from() {
+        return this.password;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(EchiApplication.class, args);
--- echi/src/main/java/com/chentao/echi/controller/SparkController.java
+++ echi/src/main/java/com/chentao/echi/controller/SparkController.java
@@ -1,18 +1,13 @@
 package com.chentao.echi.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class SparkController {
-	@Value("${neo}")
+	/*@Value("${neo}")
 	private String password;
 	
 	@RequestMapping("/password")
     public String from() {
         return this.password;
-    }
+    }*/
 	
 
 }
