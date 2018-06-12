221783e2644536a428adc509c93e6e30f8256c3c
AlettaCosima <chentao0310@126.com>
2018-06-12 09:40:50 +0800

    hystrix

--- echi/src/main/java/com/chentao/echi/EchiApplication.java
+++ echi/src/main/java/com/chentao/echi/EchiApplication.java
@@ -1,26 +1,28 @@
 package com.chentao.echi;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * 
+ * @author chentao
+ * RefreshScope 不重启服务刷新远程配置
+ * EnableEurekaClient 注册中心客户端
+ * EnableHystrixDashboard 断路器控制页面
+ * EnableCircuitBreaker 断路器
+ */
 
 @RefreshScope
-@RestController
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class EchiApplication {
 	
-	@Value("${neo}")
-	private String password;
-	
-	@RequestMapping("/")
-    public String from() {
-        return this.password;
-    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(EchiApplication.class, args);
--- echi/src/main/java/com/chentao/echi/controller/UserController.java
+++ echi/src/main/java/com/chentao/echi/controller/UserController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class UserController {
 	
@@ -12,9 +14,14 @@ public class UserController {
     private RestTemplate restTemplate;
 	
 	@GetMapping("user/hello/")
+	@HystrixCommand(fallbackMethod = "hasError")
     public String hello() {
 		return this.restTemplate.getForObject("http://localhost:8081/hello", String.class);
     }
+	
+	public String hasError() {
+		return "出错了！";
+	}
 	
 
 }
